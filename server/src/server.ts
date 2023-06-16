879f2a2f8e354b60cbdfd9c3229e008d2e361b5b
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2023-06-16 17:16:20 -0300

    Aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
-import fastify, { FastifyBaseLogger, FastifyInstance, FastifyTypeProvider, RawServerDefault } from "fastify";
+import 'dotenv/config'
+
+import fastify from "fastify";
 import { memoriesRoutes } from "./routes/memories";
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 
@@ -8,12 +12,18 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 
 app
   .listen({
     port: 3333,
+    host: '192.168.68.103',
   })
   .then(() => {
     console.log("🚀 HTTP server running on http://localhost:3333");
